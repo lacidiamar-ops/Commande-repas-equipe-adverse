81f50c6d0a089442208626c89618a9fa52379a64
lacidiamar-ops <lacidi.amar@gmail.com>
2026-09-12 17:03:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "36";
+const VERSION = "37";
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
